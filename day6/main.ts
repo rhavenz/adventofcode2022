6e7b050f34e47c5e745c558688e7c9a3c92e71db
Sebastien LE MOIGNE <sebastien.lemoigne@ambler.fr>
2022-12-06 09:54:31 +0100

    day6 part2

--- day6/main.ts
+++ day6/main.ts
@@ -1,6 +1,9 @@
 const input = await Deno.readTextFile("./input");
 
-const MARKER_LENGTH = 4;
+// * part1
+// const MARKER_LENGTH = 4;
+// * part2
+const MARKER_LENGTH = 14;
 
 let start = 0;
 let sequence = input.slice(start, start + MARKER_LENGTH);
